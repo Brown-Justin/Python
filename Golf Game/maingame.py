b9552588a71a2e704b37501c154138f0b736e339
Justin Brown <thrones42@gmail.com>
2022-08-16 02:07:32 -0500

    getting rid of test, confirmed on exp branch

--- Golf Game/maingame.py
+++ Golf Game/maingame.py
@@ -1,102 +1,53 @@
 from player import *
 import random
 import time
+from operator import attrgetter
 
 #create player objects 
-player1 = Player("Quinn", 0)
-player2 = Player("Matt", 0)
-player3 = Player("Ed", 0)
-player4 = Player("Isaac", 0)
-coursepar = 0
-playerlist = [player1, player2, player3, player4]
+playerlist = []
 
+playerlist.append( Player("Quinn", 0))
+playerlist.append( Player("Matt", 0))
+playerlist.append( Player("Ed", 0))
+playerlist.append( Player("Isaac", 0))
 
 #play golf
 def playgolf():
-	global coursepar
+	coursepar = 0
 	numberofholes = 1
-	while numberofholes < 19:
-
-		
+	while numberofholes < 10:
 
 		#every loop represents one holes of golf played
 		print("Hole Number: " + str(numberofholes))
 		
 		#first we will establish the par of the current hole, for keeping score
 		par = random.randint(3,5)
-		print("par for this hole is: " + str(par))
+		print("Par for this hole is: " + str(par))
 
-		#-----------------------
-		#player1
-		#-----------------------
-		#then each player will take a turn rolling for a swing. 
-		player1swing = random.randint(2,8)
-		print(str(player1.name) + ' took ' + str(player1swing) + ' number of strokes to finish the hole')
-
-		#now we will declare a variable to calculate the players score for this round
-		player1rs = player1swing - par
-
-		#then use this variable to update the total course score stored in the player object
-		player1.score = player1.score + player1rs
-
-		#print to console to show its working
-		print(str(player1.name) + " shot a " + str(player1rs) + " their score is now" + str(player1.score))
-		
-		#-----------------------
-		#player 2
-		#-----------------------
-
-		player2swing = random.randint(2,8)
-		print(str(player2.name) + ' took ' + str(player2swing) + ' number of strokes to finish the hole')
-		player2rs = player2swing - par
-		player2.score = player2.score + player2rs
-		print(str(player2.name) + " shot a " + str(player2rs) + " their score is now" + str(player2.score))
-
-		#-----------------------
-		#player 3
-		#-----------------------
-
-		player3swing = random.randint(2,8)
-		print(str(player3.name) + ' took ' + str(player3swing) + ' number of strokes to finish the hole')
-		player3rs = player3swing - par
-		player3.score = player3.score + player3rs
-		print(str(player3.name) + " shot a " + str(player3rs) + " their score is now" + str(player3.score))
-
-		#-----------------------
-		#player 4
-		#-----------------------
-
-		player4swing = random.randint(2,8)
-		print(str(player4.name) + ' took ' + str(player4swing) + ' number of strokes to finish the hole')
-		player4rs = player4swing - par
-		player4.score = player4.score + player4rs
-		print(str(player4.name) + " shot a " + str(player4rs) + " their score is now" + str(player4.score))
-
-		#loop iteration update
-		numberofholes = numberofholes + 1
-
-		#keep track of the total par for the course the players played on
+		for golfer in playerlist:
+			#roll a number for the players swing
+			golferswing = random.randint(2,8)
+			print( str(golfer.name) + " took " + str(golferswing) + " to complete the hole ", sep = ' ')
+			# calculate the golfers round score (rs) by subtracting par
+			golferrs = golferswing - par
+			#now change the golfers score in the golfer object
+			golfer.score = golfer.score + golferrs
+			#print feedback
+			print(str(golfer.name) + " shot a " + str(golferrs) + " their score is now" + str(golfer.score))
+			#time.sleep(1)
+		#update the total course par
 		coursepar = par + coursepar
-		time.sleep(1)
-
+		#iterate the loop
+		numberofholes = numberofholes + 1
 	else:
 		print("The round has ended.")
-		currentwinner = player1.score
-		if player2.score < currentwinner:
-			currentwinner = player2.score
-		if player3.score < currentwinner:
-			currentwinner = player3.score
-		if player4.score < currentwinner:
-			currentwinner = player4.score
-
-		print("scores:" + '\n' + str(player1.name) + " : " + str(player1.score) + '\n' + 
-			str(player2.name) + " : " + str(player2.score) + '\n' +
-			str(player3.name) + " : " + str(player3.score) + '\n' +
-			str(player4.name) + " : " + str(player4.score))
-
-		print('course par:' + str(coursepar))
-		print(str(currentwinner))
+		
+		print('Scores: ')
+		for golfer in playerlist:
+			print(str(golfer.name) + ' : ' + str(golfer.score + coursepar))
+		
+		min_attr = min(playerlist, key=attrgetter('score'))
+		print('The lowest score was: ' + str(min_attr.name) + ' with a score of: ' + str(min_attr.score + coursepar))
+		print('Total par for the course was: ' + str(coursepar))
 
 playgolf()
-
-
